32b9d5caff7ca8ac73b6824bc47caa6fa9831857
Braimah <braimahisreal@gmail.com>
2022-06-26 04:44:13 -0700

    static_lib

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: string
+ *
+ * Return: Length.
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*(s + count) != '\0')
+		count++;
+	return (count);
+}
